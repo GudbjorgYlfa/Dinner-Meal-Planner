c76274d1da25b5e8cda55fde8653171885ee196f
Gudbjorg Ylfa <gugga_ylfa@hotmail.com>
2022-03-19 15:27:54 +0000

    Created a simple NavBar and did the routing for it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <div>
-            NavBar
-        </div>
-    )
+            <nav className="navBar">
+        <ul>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Monday">
+              Monday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Tuesday">
+              Tuesday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Wednesday">
+            Wednesday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Thursday">
+              Thursday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Friday">
+            Friday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Saturday">
+            Saturday
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : '')} to="/Sunday">
+            Sunday
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+  );
 }
 
 export default NavBar
--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Monday from './components/Monday'
+import Tuesday from './components/Tuesday'
+import Wednesday from './components/Wednesday'
+import Thursday from './components/Thursday'
+import Friday from './components/Friday'
+import Saturday from './components/Saturday'
+import Sunday from './components/Sunday'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Monday" element={<Monday />} />
+        <Route path="/Tuesday" element={<Tuesday />} />
+        <Route path="/Wednesday" element={<Wednesday />} />
+        <Route path="/Thursday" element={<Thursday />} />
+        <Route path="/Friday" element={<Friday />} />
+        <Route path="/Saturday" element={<Saturday />} />
+        <Route path="/Sunday" element={<Sunday />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
